0c12b95096227429375ac08cdee18e00d4bc5ac4
AHMAD9853 <84655050+AHMAD9853@users.noreply.github.com>
2024-03-15 19:26:04 +0000

    Let, const and var

---
+++ 01_basics/declaration_Keyword.js
@@ -0,0 +1,18 @@
+let wildAnimal = "Cheetah"
+var domesticAnimal = "Cow"
+const petAnimal = "Cat"
+
+wildAnimal = "Bear"
+domesticAnimal ="Buffalo"
+
+// petAnimal ="Chicken"  // cannot reassign value
+// will get type error
+//TypeError: Assignment to constant variable.
+
+// not to use var
+
+// console.log(wildAnimal);
+// console.log(domesticAnimal);
+// console.log(petAnimal);
+
+console.log([wildAnimal,petAnimal,domesticAnimal]);
