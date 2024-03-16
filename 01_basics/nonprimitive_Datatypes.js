0851108338d80f5c71d241c683239edd44254253
AHMAD9853 <84655050+AHMAD9853@users.noreply.github.com>
2024-03-16 17:59:35 +0000

    Non Primitive Datatypes

---
+++ 01_basics/nonprimitive_Datatypes.js
@@ -0,0 +1,15 @@
+// Non Primitive Datatypes
+
+// Array
+
+let Car = ["BMW", "Honda", "Mercedes", "Toyota"]; // Datatypes = Array
+console.log(typeof Car); // Return = Object
+console.log(Car);
+
+const classroom = {   // Datatypes = Object
+    name: "Ahmad",
+    rollno: 12,
+}
+console.log(classroom.name);
+console.log(classroom.rollno);
+console.log(typeof classroom); // Return = Object
