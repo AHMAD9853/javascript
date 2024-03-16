34333f36d2e48006a641a9565adc95dadf5fdd50
AHMAD9853 <84655050+AHMAD9853@users.noreply.github.com>
2024-03-16 21:33:10 +0000

    Added Array method and diffrent way to declare Array

--- 01_basics/nonprimitive_Datatypes.js
+++ 01_basics/nonprimitive_Datatypes.js
@@ -2,14 +2,43 @@
 //Non-primitive (reference) data type
 
 // Array
+// It can be created by three ways
+// 1. By array literal
 
 let Car = ["BMW", "Honda", "Mercedes", "Toyota"]; 
 // Datatypes = Array
 //represents group of similar values
-console.log(typeof Car); 
+// console.log(typeof Car); 
 // Return = Object
-console.log(Car);
+// console.log(Car);
 
+// 2. Using new Keyword
+  let dept = new Array();
+  dept[0] = "Electrical Engineering";
+  dept[1] ="Civil Engineering",
+  dept[2]= "Electronics Engineering";
+  dept[3]="Computer Engineering";
+   
+   for (i =0;i < dept.length;i++) {
+    // console.log(dept[i]);
+    
+   }
+// 3. Using Constructor
+const emp = new Array("Ahmad","Sohel","vijay");
+for (let i = 0; i < emp.length; i++) {
+  // console.log(emp[i]);
+  
+}
+//  Array Methods
+
+// concat method
+let a=["a","b","c"]
+let A=["A","B","C"]
+// console.log(a);
+// console.log(A);
+const c=A.concat(a)
+// console.log(c);
+// console.log(a.concat(A));
 
 // object
 // it can created by 3 ways
@@ -21,9 +50,9 @@ const classroom = {
 }
 // Datatypes = Object
 //represents instance through which we can access members
-console.log(classroom.name);
-console.log(classroom.rollno);
-console.log(typeof classroom); 
+// console.log(classroom.name);
+// console.log(classroom.rollno);
+// console.log(typeof classroom); 
 // Return = Object
 
 // 2. Using new keyword
@@ -35,7 +64,7 @@ console.log(typeof classroom);
   vechile =2;
   home =2;
   
-  console.log(myObj);
+  // console.log(myObj);
 
   // 3. Using construtor
 
@@ -46,4 +75,4 @@ console.log(typeof classroom);
     
   }
   e=new city('Mumbai','10','yes')
-  console.log(e);
+  // console.log(e);
