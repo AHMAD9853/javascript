375eeb075c46c179f16182eac29b097eadb68c0e
AHMAD9853 <84655050+AHMAD9853@users.noreply.github.com>
2024-03-16 18:23:18 +0000

    add comments

--- 01_basics/primitive_Datatypes.js
+++ 01_basics/primitive_Datatypes.js
@@ -1,23 +1,28 @@
 // Primitive datatypes
 
 const uin = 21233 // Datatype = number.
+// represents numeric values
 console.log(typeof uin);
 
 const name = "Ahmad" // Datatype = string
+// represents sequence of characters
 console.log(typeof name);
 
 let marks; // Datatype = undefined " just declared not assigned any values"
+// represents undefined value
 console.log(typeof marks);
 
 let signedIn = false // Datatype = boolean  // boolean = true/false
+//represents boolean value either false or true
 console.log(typeof signedIn);
 
 let balance = BigInt(214558458159894989656); //Datatype = BigInt
 console.log(balance);
 console.log(typeof balance);
 
-let section = null // Datatype = Null but it will show as an object
-console.log(typeof section);
+let section = null // Datatype = Null 
+// represents null i.e. no value at all
+console.log(typeof section); // return = Object
 
 
 let sym = Symbol(["hello"]) // Datatype = symbol // it is used for unique value
