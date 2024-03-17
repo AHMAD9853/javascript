25a518cf00ed02f16cc97794551dbb772a813d61
AHMAD9853 <84655050+AHMAD9853@users.noreply.github.com>
2024-03-17 18:27:29 +0000

    Added comments

--- 01_basics/declaration_Keyword.js
+++ 01_basics/declaration_Keyword.js
@@ -1,3 +1,5 @@
+// Let, Var and Const
+
 let wildAnimal = "Cheetah"
 var domesticAnimal = "Cow"
 const petAnimal = "Cat"
@@ -9,7 +11,10 @@ domesticAnimal ="Buffalo"
 // will get type error
 //TypeError: Assignment to constant variable.
 
-// not to use var
+// not to use var 
+//because var can be changed from anywhere of code
+
+// let can be changed within the scope
 
 // console.log(wildAnimal);
 // console.log(domesticAnimal);
